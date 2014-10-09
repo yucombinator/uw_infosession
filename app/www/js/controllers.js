c270d4bdd7a7abc7ef23efe6a1d2951d499de0ec
Yu Chen Hou <houyuchen66@gmail.com>
2014-10-08 22:07:43 -0400

    push last changes

--- app/www/js/controllers.js
+++ app/www/js/controllers.js
@@ -1,29 +1,29 @@
 angular.module('starter.controllers', [])
 
-.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
-  // Form data for the login modal
-  $scope.loginData = {};
+.controller('AppCtrl', function($scope, $ionicModal, $timeout,$location) {
+  // Form data for the calendar modal
+  $scope.calendarData = {};
 
   // Create the login modal that we will use later
-  $ionicModal.fromTemplateUrl('templates/login.html', {
+  $ionicModal.fromTemplateUrl('templates/calendar.html', {
     scope: $scope
   }).then(function(modal) {
     $scope.modal = modal;
   });
 
   // Triggered in the login modal to close it
-  $scope.closeLogin = function() {
+  $scope.closeDateChooser = function() {
     $scope.modal.hide();
   };
 
   // Open the login modal
-  $scope.login = function() {
+  $scope.selectMonth = function() {
     $scope.modal.show();
   };
 
   // Perform the login action when the user submits the login form
-  $scope.doLogin = function() {
-    console.log('Doing login', $scope.loginData);
+  $scope.doCalendar= function() {
+    console.log('Changing date');
 
     // Simulate a login delay. Remove this and replace with your login
     // code if using a login system

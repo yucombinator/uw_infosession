f81cda7232261998585d627e9d907a3d7847a4a0
Yu Chen Hou <houyuchen66@gmail.com>
2014-10-21 12:59:47 -0400

    snazzy ui

--- app/www/js/controllers.js
+++ app/www/js/controllers.js
@@ -34,32 +34,56 @@ angular.module('starter.controllers', [])
 })
 
 .controller('EventsCtrl', function($scope,$http,$q) {
-  $scope.events = [
-    { company: 'Loading...'}
-  ];
+    $scope.loaded = false;
+    $scope.load = function(datetime){
+        $scope.current = datetime;
+        //console.log("Loading " + datetime.getFullYear()+"/"+datetime.getMonth());
+        $http.get('http://uw-infosession.herokuapp.com/api/calendar/'+datetime.getFullYear()+"/"+datetime.getMonth()+1).
+        success(function(data, status, headers, config) {
+          //$scope.events = data;
+          //console.log($scope.events);
+          //Load up more details
+          var promises = [];
+          angular.forEach(data, function(event_info) {
+           //console.log(event_info.id);
+            var request = $http.get('http://uw-infosession.herokuapp.com/api/event/'+event_info.id);
+            promises.push(request); //QUEUE THE REQUEST
+           });
+         $q.all(promises).then(function(values) {
+             //console.log(values);
+             for (var i = 0; i < values.length; ++i) {
+                data[i].details = values[i].data;
+                $scope.events.push(data[i]);
+            }
+             console.log($scope.events);
+             $scope.$parent.events = $scope.events; //pass to parent scope
+             $scope.loaded = true;
+         });
+        }).
+        error(function(data, status, headers, config) {
+          // log error
+        });
+    };
     
-  $http.get('http://uw-infosession.herokuapp.com/api/calendar/').
-    success(function(data, status, headers, config) {
-      $scope.events = data;
-      console.log($scope.events);
-      //Load up more details
-      var promises = [];
-      angular.forEach($scope.events, function(event_info) {
-       //console.log(event_info.id);
-        var request = $http.get('http://uw-infosession.herokuapp.com/api/event/'+event_info.id);
-        promises.push(request); //QUEUE THE REQUEST
-       });
-     $q.all(promises).then(function(values) {
-         //console.log(values);
-         for (var i = 0; i < values.length; ++i) {
-            $scope.events[i].details = values[i].data;
-        }
-         console.log($scope.events);
-     });
-    }).
-    error(function(data, status, headers, config) {
-      // log error
-    });
+    $scope.nextMonth = function(){
+        $scope.loaded = false;
+        $scope.current.setMonth($scope.current.getMonth()+1);
+        console.log("Load month: " + $scope.current.getMonth());
+        $scope.load($scope.current);
+    }
+    
+    //cache it locally
+    if($scope.$parent.events == null){
+        $scope.events = [];
+        $scope.load(new Date());
+    }else{
+      $scope.events = $scope.$parent.events;
+      $scope.loaded = true;
+    }
+      /* $scope.events = [
+        { company: 'Loading...'}
+      ];    */
+
     
 })
 .filter('isPast', function() {         //Date comparator
